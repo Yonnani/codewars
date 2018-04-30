9feb825c67b39973a5b6dad8be7a7409f9a07bb9
Yonnani <iamai5206@gmail.com>
2018-05-01 00:01:32 +0900

    duplicateEncode

---
+++ duplicateEncode.js
@@ -0,0 +1,16 @@
+function duplicateEncode(word){
+    var word_uppercase = word.toUpperCase();
+    var word_arr = word_uppercase.split('');
+    var encoded_str =
+        word_arr.map((ele, idx) => {
+            var returnVal = "(";
+            var firstIdx = word_arr.indexOf(ele);
+            var lastIdx = word_arr.lastIndexOf(ele);
+            if (firstIdx != lastIdx) {
+                returnVal = ")";
+            }
+            return returnVal;
+        }).join('');
+
+    return encoded_str;
+}
