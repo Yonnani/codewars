b840e4e836011593dea9d2891cbb1d73edf4ddf0
Yonnani <iamai5206@gmail.com>
2018-05-01 00:12:05 +0900

    duplicateEncode - 안 쓰는 idx 제거

--- duplicateEncode.js
+++ duplicateEncode.js
@@ -2,7 +2,7 @@ function duplicateEncode(word){
     var word_uppercase = word.toUpperCase();
     var word_arr = word_uppercase.split('');
     var encoded_str =
-        word_arr.map((ele, idx) => {
+        word_arr.map(ele => {
             var returnVal = "(";
             var firstIdx = word_arr.indexOf(ele);
             var lastIdx = word_arr.lastIndexOf(ele);
