a60cfd6fab33ce94caf87cebe1099562aa38a4c8
Yonnani <iamai5206@gmail.com>
2018-10-04 00:18:03 +0900

    Vasya - Clerk.js 버그 수정

--- Vasya - Clerk.js
+++ Vasya - Clerk.js
@@ -1,32 +1,39 @@
 function tickets(peopleInLine){
-    let vasaya100 = 0;
-    let vasaya50 = 0;
-    let vasaya25 = 0;
+    const totalCash = {
+        25: 0, 50: 0, 100: 0
+    };
+    const ticketPrice = 25;
   
     const result = 
         peopleInLine.every(cash => {
             let thisResult = true;
-            if (cash === 25) {
-                vasaya25++; 
-            } else if (cash === 50) {
-                vasaya50++;
-                if (vasaya25 > 0) {
-                    vasaya25--;
-                } else {
-                    thisResult = false;
-                }
-            } else {
-                vasaya100++;
-                if (vasaya50 > 0 && vasaya25 > 0) {
-                    vasaya50--;
-                    vasaya25--;
-                } else {
-                    thisResult = false;
-                }
+            
+            const keys = Object.keys(totalCash);
+
+            // cash 받기
+            const thisKey = keys.filter(aKey => parseFloat(aKey) === cash)[0];
+            totalCash[thisKey]++;
+
+            let leftChange = cash - ticketPrice;
+            // 거스름돈 주기
+            if (leftChange > 0) {
+                keys.sort((a, b) => parseFloat(b) - parseFloat(a));
+                const changeCandidates = keys.filter(key => key < cash);
+
+                changeCandidates.forEach(change => {
+                    while(leftChange > 0 && totalCash[change] > 0) {
+                        leftChange -= parseFloat(change);
+                        totalCash[change]--;
+                    }
+                });
+            }
+
+            if (leftChange > 0) {
+                thisResult = false;
             }
   
             return thisResult;
         });
   
     return result ? "YES" : "NO";
-  }
+}
